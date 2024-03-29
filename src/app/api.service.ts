278351f835bbc30bf80e09fcc026e6e86337f5b9
Yaseen Feroz <mdyaseenferoz@outlook.com>
2024-03-29 17:33:39 +0530

    Update api.service.ts

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class ApiService {
-  private apiUrl = 'http://localhost:3000'; // Replace with your API endpoint
+  private apiUrl = 'https://cric-be.onrender.com'; // Replace with your API endpoint
 
   constructor(private http: HttpClient) {}
 
